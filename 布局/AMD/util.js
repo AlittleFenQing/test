dd303b8f1f4ffc6e5370f93d78f9e6d50bfa6020
AlittleFenQing <1443217645@qq.com>
2018-05-28 21:01:29 +0800

    AMD

---
+++ 布局/AMD/util.js
@@ -0,0 +1,13 @@
+define(function() {
+    var util={
+        getFormatDate:function(date,type){
+            if(type===1){
+                return '2018-05-28'
+            }
+            if(type===2){
+                return '2018年-5月-28日'
+            }
+        }
+    }
+    return util;
+});
